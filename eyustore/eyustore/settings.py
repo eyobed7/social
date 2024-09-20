a0e4ee4657f9d84ec841c0ad0e6c242fa0282864
eyobeds <sebrexam2009@gmail.com>
2024-09-20 10:54:54 +0300

    new change has

--- eyustore/eyustore/settings.py
+++ eyustore/eyustore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3_q!btsl7=@wfx00ua#maay&qz9o592)4g*7hh@_%_)yp+sw81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
